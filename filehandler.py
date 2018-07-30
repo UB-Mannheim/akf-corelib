076a0a5b4fb2bf64e1309603bffc70817d9c52e6
johannes <johannesst@gmx.de>
2018-07-30 19:04:47 +0200

    added filehandler for simple file operations

---
+++ filehandler.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+class FileHandler(object):
+    """
+    Utility class for performing various default file operations.
+
+    """
+
+    @staticmethod
+    def delete_directory_tree(path):
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+
+    @staticmethod
+    def create_directory_tree(path):
+        os.makedirs(path)
